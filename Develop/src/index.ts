cf0d4406472321dc787f406ebe7233ef225cd0a1
nita9801 <mrschristinemlopez@gmail.com>
2025-01-19 20:35:17 -0500

    fixing the cli in index

--- Develop/src/index.ts
+++ Develop/src/index.ts
@@ -1,19 +1,37 @@
 // import classes
 
-import { Vehicle } from "./classes/Vehicle";
 import { Motorbike } from"./classes/Motorbike";
 import { car } from "./classes/car";
 import { Wheel } from "./classes/Wheel";
-import { Cli }  from "./classes/Cli";
+import { Cli as ImportedCli }  from "./classes/Cli";
 import { Truck } from"./classes/Truck";
 
 // create an array of vehicles
-const vehicle: Vehicle [] = [];
-const truck1 = new Truck(Cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
+const vehicle: (car | Motorbike | Truck)[] = [];
+
+class LocalCli {
+  generateVin(): string {
+    throw new Error("Method not implemented.");
+  }
+  private vehicles: (car | Motorbike | Truck)[];
+
+  constructor(vehicles: (car | Motorbike | Truck)[]) {
+    this.vehicles = vehicles;
+  }
+  // start the cli
+  startCli() {
+    console.log("CLI started with vehicles:", this.vehicles);
+  }
+}
+
+// Create an instance of the Cli class
+const cli = new LocalCli(vehicle);
+
+const truck1 = new Truck(cli.generateVin(),"red", "Ford", "F-150", 2021, 5000, 120, [], 10000);
 
 // will use default wheels
 const car1 = new car(
-  Cli.generateVin(),
+  cli.generateVin(),
   'blue',
   'Toyota',
   'Camry',
@@ -22,16 +40,12 @@ const car1 = new car(
   130,
   []
 );
- const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
- const motorbike1 = new Motorbike(Cli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels,0);
+
+const motorbike1Wheels = [new Wheel(17, "Michelin"), new Wheel(17, "Michelin")];
+
+const motorbike1 = new Motorbike(ImportedCli.generateVin(), "black", "Harley Davidson", "Sportster", 2021, 500, 125, motorbike1Wheels,0);
 
 // push vehicles to array
  vehicle.push(truck1);
  vehicle.push(car1);
  vehicle.push(motorbike1);
-
-// create a new instance of the Cli class
- const cli = new Cli(vehicle);
-
- // start the cli
-cli.startCli();
